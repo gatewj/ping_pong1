123b105b221a1926fc017e97f75bf46213f8fde7
Anjali Shah <shah58@scarletmail.rutgers.edu>
2016-03-25 15:38:35 -0400

    Updated to fix error for different keys with same value. This code works for all Leetcode testcases

--- WordPattern.py
+++ WordPattern.py
@@ -10,20 +10,23 @@ class Solution(object):
         pat_len = len(pattern)
         str_len = len(words)
         
-        if pat_len != str_len
-            return false
+        if pat_len != str_len:
+            return False
         else:
             i = 0
             j = 0
             while i < pat_len and j < str_len:
                 if pat_word_hash.has_key(pattern[i]) == False:
-                    pat_word_hash[pattern[i]]: words[j]
+                    if words[j] in pat_word_hash.values():
+                        return False
+                    pat_word_hash[pattern[i]] = words[j]
                     i = i + 1
                     j = j + 1
                 else:
                     if pat_word_hash.get(pattern[i]) != words[j]:
-                        return FALSE
+                        return False
                     else:
                         i = i + 1
                         j = j + 1
-            return TRUE
+            print pat_word_hash
+            return True
